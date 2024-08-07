fc4a07f7ea05eec7919547fcbe11e6387c36874c
Olajide <clickcom007@yahoo.com>
2024-08-07 11:56:31 +0100

    add dark mode

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -8,6 +8,8 @@ export default function DarkModeSwitch() {
     const { theme, setTheme, systemTheme } = useTheme();
     const currentTheme = theme ==='system' ? systemTheme : theme;
   return (
-    <div>{currentTheme === 'dark' ? <MdLightMode className='text-xl cursor-pointer hover:text-amber-500'/> : <MdDarkMode className='text-xl cursor-pointer hover:text-amber-500'/>}</div>
+    <div>{currentTheme === 'dark' ? <MdLightMode onClick={()=>setTheme ('light')}
+      className='text-xl cursor-pointer hover:text-amber-500'/> : <MdDarkMode onClick={()=>setTheme ('dark')} 
+      className='text-xl cursor-pointer hover:text-amber-500'/>}</div>
   )
 }
